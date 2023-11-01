a8e1a855902e21db1c9c71ac49d6865199e0c76e
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-01 15:52:27 +0530

    Add files via upload

---
+++ Arrayssyntax4.java
@@ -0,0 +1,22 @@
+package demo.arrays;
+
+import java.util.Arrays;
+
+public class Arrayssyntax4 {
+
+	public static void main(String[] args) {
+		                                                    
+	
+	    // static intialisation
+	    int[] monthdays = {31,28,31,30,31,30,31,31,30,31,30,31,};
+	
+	    System.out.println("pril has: "+ monthdays[3] + "days");
+	
+	    System.out.println(Arrays.toString(monthdays));
+	
+	    System.out.println(monthdays.length);
+	    
+	
+	}
+
+}
