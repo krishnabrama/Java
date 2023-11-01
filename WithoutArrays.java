4e73ee02e43b4bb10d15e952dcfa676dc5f353d8
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-01 14:43:55 +0530

    Add files via upload

---
+++ WithoutArrays.java
@@ -0,0 +1,26 @@
+package demo.arrays;
+
+public class WithoutArrays {
+
+	public static void main(String[] args) {
+		
+		 String name1 = "anil"; 
+		 String name2 = "nagi";
+		 String name3 = "ajun";
+		 String name4 = "ravi";
+		 String name5 = "mahindra";
+			 
+	     System.out.println("name1");
+	     System.out.println("name2");
+	     System.out.println("name3");
+	     System.out.println("name4");
+	     System.out.println("name5");
+				
+		 String [] name = {"anil", "nagi", "arjun", "ravi", "mahindra"};
+  	     for(String names: name)  {
+		  System.out.println(names);  
+  	     }	 
+		 
+	}
+
+}
---
+++ homogeniousData.java
@@ -0,0 +1,24 @@
+package demo.arrays;
+
+public class homogeniousData {
+
+	public static void main(String[] args) {
+	
+		 double[] ages = {75,71,45,41,12,11}; 
+		
+		 int[]  weight = {78, 76, 45,54,};
+		
+	    String[] names = {"jai","raj","rajesh",};
+		
+		boolean[] availability = {true,false};
+
+	    System.out.println(names[0]);
+	    System.out.println(names[1]);
+	    System.out.println(names[2]);
+	    System.out.println(names[3]);
+		
+		
+		
+	}
+
+}
