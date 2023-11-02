f5874e8c615844a918eea813c1b3c856e403f584
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-02 15:18:20 +0530

    Add files via upload

---
+++ SingleifStatement.java
@@ -0,0 +1,25 @@
+package selection;
+
+public class SingleifStatement {
+
+	public static void main(String[] args) {
+	
+		String action = "go to party";
+		boolean isRainingHeavily =false;
+
+		if(isRainingHeavily) {
+			action ="can not go to the party";
+		}
+		
+		
+		
+		
+		System.out.println(action);
+		
+		
+		
+	}
+	
+	
+
+}
