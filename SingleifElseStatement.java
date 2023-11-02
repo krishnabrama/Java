a85ec17b5cfa579eb21e302022a9ffe599ad08d1
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-02 15:19:20 +0530

    Add files via upload

---
+++ SingleifElseStatement.java
@@ -0,0 +1,25 @@
+package selection;
+
+//find wether the given number is even or odd
+
+public class SingleifElseStatement {
+
+	public static void main(String[] args) {
+	
+		int number = 12;
+		
+		if(number%2 == 0) {
+			System.out.println(number +"is an even number");
+		} else {
+			System.out.println(number +"is an odd number");
+
+		
+			
+			
+		}
+		
+		
+		
+	}
+
+}
