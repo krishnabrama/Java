b30262a730b4c510130da70d0f247637168a4c79
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-17 11:33:21 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,29 @@
+package bynary.operators;
+
+public class Vowels {
+
+	public static void main(String[] args) {
+		
+				
+		
+	float r1,x1,x=925.583f,r2,y1,y=725.583f;
+		
+		x1 = x * 2000;// 25.589 *1000----25583
+		r1 = x1 % 2000;// 25583 % 1000---583
+		
+		y1= y * 2000;
+		r2 = y1 % 2000;// 589
+		if(r1 == r2) {
+			System.out.println("they are same");
+		}
+		else {
+			System.out.println("not same");
+		}
+		
+		
+		
+			
+
+	}
+
+}
