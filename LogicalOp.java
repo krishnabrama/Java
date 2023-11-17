01076a70f1ff6ef7e351222a96ca0da814d7428e
krishnabrama <148056934+krishnabrama@users.noreply.github.com>
2023-11-17 11:52:00 +0530

    Add files via upload

---
+++ LogicalOp.java
@@ -0,0 +1,28 @@
+package bynary.operators;
+
+public class LogicalOp {
+
+	public static void main(String[] args) {
+	
+		int x=864,y=584,z=250;
+		// && || !
+		
+		if( (x>y) && (x>z)) {
+			System.out.println("X is Big");
+		}
+		else if(y>z) {
+			System.out.println("y is big");
+		}
+		else {
+			System.out.println("Z is bIg");
+		}
+				System.out.println("Program Ends");
+	}
+
+
+		
+		
+
+	}
+
+
